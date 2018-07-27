f240dedab72021566eebbea4f93764900ec33cd8
oneill45 <oneill4511@gmail.com>
2018-07-26 22:41:27 -0400

    Test added for calculate total

--- BabysitterKata/BabysitterKataTest/BabysitterKataTest.cs
+++ BabysitterKata/BabysitterKataTest/BabysitterKataTest.cs
@@ -7,10 +7,16 @@ namespace BabysitterKataTest
     public class BabysitterKataTest
     {
         [Fact]
-        public void ItShouldReturnCorrectRateByHour()
+        public void ShouldReturnCorrectRateByHour()
         {
             Babysitter babysitter = new Babysitter();
             Assert.Equal(12.0, babysitter.CalculateHourlyRate(new TimeSpan(20, 0, 0)));
+        }
+        [Fact]
+        public void ShouldReturnCorrectTotal()
+        {
+            Babysitter babysitter = new Babysitter();
+            Assert.Equal(36.0, babysitter.CalculateTotal(new TimeSpan(17, 0, 0), new TimeSpan(2, 0, 0)));
         }
     }
 }
