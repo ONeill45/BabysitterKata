bf923278f020c6240330de9d603477f64c3701cc
scott dougherty <scott.dougherty@macys.com>
2018-08-08 22:20:19 -0400

    Revert "Changed project to console app"
    This reverts commit bb252986cc2b8f366d7d7d899af2aff0571ad1f5.
    
    Reverted to recover test project

--- BabysitterKata/BabysitterKata/Program.cs
+++ BabysitterKata/BabysitterKata/Program.cs
@@ -1,28 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace BabysitterKata
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine();
-            Babysitter babysitter = new Babysitter();
-
-            Console.WriteLine($"Hello {name}!");
-            Console.WriteLine("My name is Bee. I heard you were in need of a babysitter tonight?");
-
-            Console.WriteLine("What time would you need me to start?");
-            string startInput = Console.ReadLine();
-            
-            Console.WriteLine("Okay and what time would you be home?");
-            string endInput = Console.ReadLine();
-
-            DateTime start = DateTime.Parse(startInput);
-            DateTime end = DateTime.Parse(endInput);
-
-            Console.Read();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
---
+++ BabysitterKata/BabysitterKataTest/BabysitterKataTest.cs
@@ -0,0 +1,50 @@
+using System;
+using Xunit;
+using BabysitterKata.Models;
+
+namespace BabysitterKataTest
+{
+    public class BabysitterKataTest
+    {
+        private Babysitter BabysitterUnderTest { get; set; }
+        public BabysitterKataTest()
+        {
+            BabysitterUnderTest = new Babysitter();
+        }
+        [Fact]
+        public void ShouldReturnCorrectRateByHour()
+        {
+            //bedtime must be defined to calculate hourly rate properly
+            BabysitterUnderTest.BedTime = new TimeSpan(21, 0, 0);
+            Assert.Equal(12.0, BabysitterUnderTest.CalculateHourlyRate(new TimeSpan(20, 0, 0)));
+        }
+        [Fact]
+        public void ShouldReturnCorrectTotal()
+        {
+            BabysitterUnderTest.StartTime = new TimeSpan(18, 0, 0);
+            BabysitterUnderTest.EndTime = new TimeSpan(3, 0, 0);
+            BabysitterUnderTest.BedTime = new TimeSpan(21, 0, 0);
+            Assert.Equal(108.0, BabysitterUnderTest.CalculateTotal());
+        }
+        [Fact]
+        public void ShouldValidateStartAndEnd()
+        {
+            Assert.True(BabysitterUnderTest.ValidateStartAndEnd(DateTime.Parse("5:00 PM").TimeOfDay, DateTime.Parse("10:00 PM").TimeOfDay));
+            Assert.False(BabysitterUnderTest.ValidateStartAndEnd(DateTime.Parse("5:00 PM").TimeOfDay, DateTime.Parse("5:00 PM").TimeOfDay));
+            Assert.True(BabysitterUnderTest.ValidateStartAndEnd(DateTime.Parse("5:00 PM").TimeOfDay, DateTime.Parse("2:00 AM").TimeOfDay));
+        }
+        [Fact]
+        public void ShouldReturnCorrectMessage()
+        {
+            Babysitter babysitter = new Babysitter();
+            string message = babysitter.Babysit(new TimeSpan(18, 0, 0), new TimeSpan(3, 0, 0), new TimeSpan(21, 0, 0));
+            Assert.Equal("Sure I can babysit from 06:00 PM to 03:00 AM. That will be $108", message);
+        }
+        [Fact]
+        public void ShouldValidateBedTime()
+        {
+            BabysitterUnderTest.StartTime = new TimeSpan(17, 0, 0);
+            Assert.True(BabysitterUnderTest.ValidateBedTime(new TimeSpan(21, 0, 0)));
+        }
+    }
+}
